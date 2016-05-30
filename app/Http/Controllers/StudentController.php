75362e8030b40098b9928390b129394d13bb8819
Alexandr Vasiliev <alexandr.vasiliev1994@gmail.com>
2016-05-30 21:23:01 +0300

    change students index view

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -19,14 +19,13 @@ class StudentController extends Controller
 
     public function index(Requests\BaseRequest $request)
     {
-//        \DB::enableQueryLog();
         if ($request->has('search')) {
             $students = $this->studentRepository->pushCriteria(new StudentSearchCriteria($request->get('search')));
         } else {
             $students = $this->studentRepository;
         }
-        $students = $students->paginate();
-//dd(\DB::getQueryLog());
+        $students = $students->paginate(40);
+
         return view('student.index', [
             'students' => $students,
             'search' => $request->get('search', null),
--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -16,24 +16,44 @@
         </div>
     </div>
     @include('blocks.search_user', ['route' => 'student.index'])
-    <table class="table">
+    <table class="table table-striped">
         <thead>
             <tr>
-                <th>Name</th>
-                <th>Actions</th>
+                <th colspan="4">Names</th>
             </tr>
         </thead>
         <tbody>
-            @foreach($students as $student)
+            <?php $count = count($students); ?>
+            @for($i = 0; $i < $count; $i=$i+4)
                 <tr>
                     <td>
-                        <a href="{!! route('student.show', [$student->id]) !!}">
-                            {!! $student->getFullName() !!}
+                        <a href="{!! route('student.show', [$students[$i]->id]) !!}">
+                            {!! $students[$i]->getFullName() !!}
                         </a>
                     </td>
-                    <td></td>
+                    @if (isset($students[$i+1]))
+                        <td>
+                            <a href="{!! route('student.show', [$students[$i+1]->id]) !!}">
+                                {!! $students[$i+1]->getFullName() !!}
+                            </a>
+                        </td>
+                    @endif
+                    @if (isset($students[$i+2]))
+                        <td>
+                            <a href="{!! route('student.show', [$students[$i+2]->id]) !!}">
+                                {!! $students[$i+2]->getFullName() !!}
+                            </a>
+                        </td>
+                    @endif
+                    @if (isset($students[$i+3]))
+                        <td>
+                            <a href="{!! route('student.show', [$students[$i+3]->id]) !!}">
+                                {!! $students[$i+3]->getFullName() !!}
+                            </a>
+                        </td>
+                    @endif
                 </tr>
-            @endforeach
+            @endfor
         </tbody>
     </table>
     {!! $students->render() !!}
